15983030bd9e39def6e7943d9850bcf1f1061504
timurmisc <75980987+timurmisc@users.noreply.github.com>
2020-12-15 15:05:02 -0500

    first commmit through eclipse

---
+++ firstProject/src/firstProject/firstClass.java
@@ -0,0 +1,18 @@
+package firstProject;
+
+import org.junit.Test;
+
+public class firstClass {
+	
+	@Test
+	public void firstMethod() {
+		
+		int i = 100;
+		++i;
+		
+		
+		
+		System.out.println(i);
+	}
+
+}
